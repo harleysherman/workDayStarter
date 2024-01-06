d7661644dbe7ab8f399ce6d6ae413d49208b8ea7
harleysherman <harsatr11@icloud.com>
2024-01-05 18:41:28 -0600

    Revised js eventlistener logic, added local storage, revised readme

--- Develop/script.js
+++ Develop/script.js
@@ -5,51 +5,70 @@
 
 //Global variables
 var today = dayjs();
-var pastHTML = document.getElementsByClassName('past');
-var presentHTML = document.getElementsByClassName('present');
-var futureHTML = document.getElementsByClassName('future');
+// var pastHTML = document.querySelector('past');
+// var presentHTML = document.querySelector('present');
+// var futureHTML = document.querySelector('future');
+// var timeBlock = document.querySelector('.time-block');
 //var currentDayDate;
 var hoursIndex = 0;
-var hours = [
-  $('hour-9'),
-  $('hour-10'),
-  $('hour-11'),
-  $('hour-12'),
-  $('hour-13'),
-  $('hour-14'),
-  $('hour-15'),
-  $('hour-16'),
-  $('hour-17'),
-]
-var currentTimeHTML = document.getElementsByClassName('time-block');
-var currentTime = $('#currentDay').text(today.format('hhA'));
-// var rootEl = $(".timeBlock");
-var hourEl = $(".timeBlock").childern(".hour").childern(".description");
-//console.log(hours);
+var hours = [9, 10, 11, 12, 13, 14, 15, 16, 17]
+var currentTimeHTML = $('.time-block');
+//var currentTime = $('#currentDay').text(today.format('hhA'));
+var currentHour = parseInt(dayjs().format('H'));
+//var rootEl = $(".time-block");
+var hourEl = currentTimeHTML.children(".hour").children(".description").text;
+// console.log(rootEl);
+console.log(hours);
+console.log(currentTimeHTML);
 
 $(function () {
+  console.log("We're in the function!!");
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
   // function? How can DOM traversal be used to get the "hour-x" id of the
   // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
+  $('.saveBtn').on('click', function() { 
+    console.log("We're in save button!!");
+    var userInput = $(this).siblings().eq(1).val();
+    var parentSaveBtn = $(".saveBtn").parent().attr("id");
+    console.log(userInput);
+    console.log(parentSaveBtn);
+
+    //console.log($(event.target).siblings().eq(1).val()); //this is the value in the setItems()
+    localStorage.setItem(parentSaveBtn, userInput);
+  });
+
+  $("#hour-9 .description").val(localStorage.getItem("hour-9"));
+  $("#hour-10 .description").val(localStorage.getItem("hour-10"));
+  $("#hour-11 .description").val(localStorage.getItem("hour-11"));
+  $("#hour-12 .description").val(localStorage.getItem("hour-12"));
+  $("#hour-13 .description").val(localStorage.getItem("hour-13"));
+  $("#hour-14 .description").val(localStorage.getItem("hour-14"));
+  $("#hour-15 .description").val(localStorage.getItem("hour-15"));
+  $("#hour-16 .description").val(localStorage.getItem("hour-16"));
+  $("#hour-17 .description").val(localStorage.getItem("hour-17"));
+
   // TODO: Add code to apply the past, present, or future class to each time
   // block by comparing the id to the current hour. HINTS: How can the id
   // attribute of each time-block be used to conditionally add or remove the
   // past, present, and future classes? How can Day.js be used to get the
   // current hour in 24-hour time?
   //
-  //Past 24 Hour Clock
-  //$('pastHTML').text(today.format('Z'));
 
-  //Present 24 Hour Clock
-  $('presentHTML').text(today.format('Z'));
-  if (currentTime == hourEl) {
-    presentHTML.classList.add("present");
+  //for loop for current time
+  for(let i = 0; i < currentTimeHTML.length; i++) {
+    //If current hour is present, make hour present
+    if (currentHour === hours[i]) {
+      $(currentTimeHTML[i]).addClass("present");
+    } else if(currentHour > hours[i]) {
+      $(currentTimeHTML[i]).addClass("past");
+    }else {
+      //future condition
+      $(currentTimeHTML[i]).addClass("future");
+    }
   }
-  
+
 
   //Future 24 Hour Clock
   //$('futureHTML').text(today.format('Z'));
@@ -57,10 +76,9 @@ $(function () {
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
-  //localStorage.setItem(JSON.strigify("key")) 
-  //localStorage.getItem("Key", JSON.parse("string var"))
+  
 
 
   // TODO: Add code to display the current date in the header of the page.
-  $('#currentDay').text(today.format('MMM D, YYYY'));
+  $('#currentDay').text(today.format('dddd MMM D, YYYY'));
 });
--- README.md
+++ README.md
@@ -4,10 +4,10 @@
 
 Provide a short description explaining the what, why, and how of your project. Use the following questions as a guide:
 
-- What was your motivation?
-- Why did you build this project? (Note: the answer is not "Because it was a homework assignment.")
-- What problem does it solve?
-- What did you learn?
+- What was your motivation? The goal of the project is to create a day planner for work.
+- Why did you build this project? (Note: the answer is not "Because it was a homework assignment.") The project taught me how to use JQuery better.
+- What problem does it solve? The project helped solved some organizational uses during the work day.
+- What did you learn? I learn how to use local storage and better use of JQuery.
 
 ## Table of Contents (Optional)
 
@@ -20,7 +20,7 @@ If your README is long, add a table of contents to make it easy for users to fin
 
 ## Installation
 
-What are the steps required to install your project? Provide a step-by-step description of how to get the development environment running.
+To install, please download from GitHub and implement on your own terminal.
 
 ## Usage
 
@@ -34,34 +34,26 @@ To add a screenshot, create an `assets/images` folder in your repository and upl
 
 ## Credits
 
-List your collaborators, if any, with links to their GitHub profiles.
-
-If you used any third-party assets that require attribution, list the creators with links to their primary web presence in this section.
-
-If you followed tutorials, include links to those here as well.
+I want to thank Tomas and Miah for their help during study group. I want to also thank Leif for his patience for explaining JQuery.
 
 ## License
 
-The last section of a high-quality README file is the license. This lets other developers know what they can and cannot do with your project. If you need help choosing a license, refer to [https://choosealicense.com/](https://choosealicense.com/).
+The license is MIT license.
 
 ---
 
-🏆 The previous sections are the bare minimum, and your project will ultimately determine the content of this document. You might also want to consider adding the following sections.
-
 ## Badges
 
-![badmath](https://img.shields.io/github/languages/top/lernantino/badmath)
-
-Badges aren't necessary, per se, but they demonstrate street cred. Badges let other developers know that you know what you're doing. Check out the badges hosted by [shields.io](https://shields.io/). You may not understand what they all represent now, but you will in time.
+There are no badges at this time.
 
 ## Features
 
-If your project has a lot of features, list them here.
+The features are: entering in events at a time and displaying the current hour.
 
 ## How to Contribute
 
-If you created an application or package and would like other developers to contribute it, you can include guidelines for how to do so. The [Contributor Covenant](https://www.contributor-covenant.org/) is an industry standard, but you can always write your own if you'd prefer.
+There are no contributions at this time.
 
 ## Tests
 
-Go the extra mile and write tests for your application. Then provide examples on how to run them here.
+There are no tests at this time. Good luck with organization.
